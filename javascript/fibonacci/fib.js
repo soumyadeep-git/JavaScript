8b3ae7f231316c0631e9534635b05fe795318224
soumyadeep-git <84786718+soumyadeep-git@users.noreply.github.com>
2021-07-22 08:24:52 +0530

    Update fib.js

--- javascript/fibonacci/fib.js
+++ javascript/fibonacci/fib.js
@@ -3,7 +3,7 @@
 let firstIndex = 0, secondIndex = 1, count, nextDigit;
 console.log(firstIndex);
 console.log(secondIndex);
-for (count = 0; count <= 10; count++) {    //10 here means that it will iterate the loop will iterate upto 10 times 
+for (count = 0; count <= 10; count++) {    //10 here means that it will iterate the loop upto 10 times 
     nextDigit = firstIndex + secondIndex;
     console.log(nextDigit);
     firstIndex = secondIndex;
